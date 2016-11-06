9ec3c974511fd82225cf1bd15c4d0158935f8160
Bruno Alvarenga <brunoviver10@gmail.com>
2016-11-06 15:48:11 -0200

    Soma Aritimetica de resultado

---
+++ aula05.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+
+    $n1 = 3;
+    $n2 = 2;
+    $s  =  $n1 + $n2 ;
+
+ echo "A soma vale $s" ;
+
+
+
+
+    ?>
+</div>
+</body>
+</html>
+
