0deb6a52484ff62ba525ae2c81f19fe7e87f8360
Bruno Alvarenga <brunoviver10@gmail.com>
2016-11-07 13:42:31 -0200

    Esse tem o novo arquivo  demo file

--- funcoesaritimeticas.php
+++ funcoesaritimeticas.php
@@ -11,18 +11,21 @@ Abs pega o valor absoluto do elemento
 <div>
 
     <style>
-        {
+      h2  {
             font-color: #2539ee;
-            color: #2539ee;
+            color: #2539ee
+        ;
         }
     </style>
     <?php
 
 
 
-    $v1 =$_GET["X"];
-    $v2=$_GET["Y"];
-    ECHO "<H2>Valorea recebidos $v1 e $v2</H2>";
+    $v1 = $_GET["x"];
+    $v2 = $_GET["y"];
+    echo "<H2>Valorea recebidos: $v1 e $v2</H2>";
+    echo "O valor absoluto de $v2 e " . abs($v2);
+   "o valor de $v1 em modeda e R$". number_format($v1, 2);
 
 
 
